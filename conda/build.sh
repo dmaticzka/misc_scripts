c7782a641e2bffe3bea8dbccb38ce811ccd7081c
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-06-11 17:28:51 +0200

    remove debug statements

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,3 @@
-echo SRC_DIR
-echo $SRC_DIR
-ls -lh $SRC_DIR
-
 mkdir -p $PREFIX/bin
 
 PROGS="multiBedSummary.py peakachu_initial_peaks_to_bed.R prepare_graphprot_seqs.py runjamm_from_bed6.sh simple_deseq2.R yass_reverse_repeats_to_bed12.R"
