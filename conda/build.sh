d54577b1c95bd9befbf3529cc56f21752fccba79
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-06-11 11:59:28 +0200

    start automatic upload to anaconda

---
+++ conda/build.sh
@@ -0,0 +1,9 @@
+mkdir -p $PREFIX/bin
+
+PROGS="multiBedSummary.py peakachu_initial_peaks_to_bed.R prepare_graphprot_seqs.py runjamm_from_bed6.sh simple_deseq2.R yass_reverse_repeats_to_bed12.R"
+
+# manually distribute files
+for PROG in PROGS
+do
+    cp -v $PROG $PREFIX/bin/
+done
