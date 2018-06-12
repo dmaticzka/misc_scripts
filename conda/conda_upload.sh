59f1dc1ef742f12235197ab53f7cfccd9c44d770
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2018-06-12 10:49:41 +0200

    build multiple python versions

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -2,10 +2,17 @@
 PKG_NAME=misc_scripts
 USER=dmaticzka
 
+echo $TRAVIS_PYTHON_VERSION
+
 OS=linux-64
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=`date +%Y.%m.%d`
 conda build . && \
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+anaconda ´
+-t $CONDA_UPLOAD_TOKEN upload \
+-u $USER \
+-l nightly \
+--python $TRAVIS_PYTHON_VERSION \
+$CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
